153d23677a56dba1468514dc6aac5f2b0d817069
klameer@yahoo.com <klameer@yahoo.com>
2024-11-04 11:46:26 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-from functions_anaplan import download_export_as_df
+from functions_anaplan import list_exports
 
-[{'id': '116000000237', 'name': 'EXP: Export Revenue', 'exportType': 'GRID_CURRENT_PAGE', 'exportFormat': 'text/csv', 'encoding': 'UTF-8', 'layout': 'GRID_CURRENT_PAGE'}]
-
-df = download_export_as_df(exportId='116000000237', )
-print(df.columns)
